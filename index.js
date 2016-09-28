bbc347057f039d7ecb20f425b59f9ae8a97c15cf
goto shunya <shunya.gotoh@gmail.com>
2016-09-28 23:37:44 +0900

    順番に当番を指定する仕組みを更新

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+//Botに必要なモジュール各種
+
 var Botkit = require('botkit');
 var Holiday = require('holiday');
 var CronJob = require('cron').CronJob;
 var controller = Botkit.slackbot();
-var dateInstance = new Date();
-var month = ("0" + (dateInstance.getMonth()+1).toString(10)).slice(-2);
-var day = ("0" + (dateInstance.getDate()).toString(10)).slice(-2);
-var date = month + day;
+
+//日付取得関数
+var getDate = function(date){
+    var month = ("0" + (date.getMonth()+1).toString(10)).slice(-2);
+    var day = ("0" + (date.getDate()).toString(10)).slice(-2);
+    return  month + day;
+}
+//担当者名リスト
+var array = [ 'A' , 'B' , 'C'];
+
+//循環させる形で添字を返す関数
+var getCurrentIdx = (function(){
+    var current = 0;
+    return function(length){
+        return current++ % length;
+    }
+})();
+
 var bot = controller.spawn({
-      token: process.env.token
+      token: 'token' 
 }).startRTM(function(err,bot,payload) {
     // 初期処理
     if (err) {
         throw new Error('Could not connect to Slack');
     }
+    //定期内容
     new CronJob({
         cronTime: '* * * * 1-5',
         onTick: function() {
-            if(Holiday.isExist('date')){
+            var date = getDate(new Date());
+            if(!Holiday.isExist('date')){
                 bot.say({
                         channel: 'team_test',
-                        text: '祝日',
-                        username: '祝日通知',
+                        text: '当番は' + array[getCurrentIdx(array.length)] + 'です',
+                        username: '当番通知',
                         icon_url: ''
                 });
             }
